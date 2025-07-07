393570aab3d7d2f15a3d40a082dd13bc56ca74d0
ulim006 <mulaebi8@gmail.com>
2025-07-07 15:25:43 +0900

    유림 UI 확인

---
+++ src/pages/CommuPage.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import { StyledParagraph } from "./CommuPage.styles.js"
+
+function CommuPage() { 
+    const [count, setCount] = useState(0)
+  
+    return (
+      <>
+        <StyledParagraph>
+          유림
+        </StyledParagraph>
+      </>
+    )
+  }
+
+export default CommuPage;
---
+++ src/pages/CommuPage.styles.js
@@ -0,0 +1,7 @@
+// CommuPage.styles.js 예시
+import styled from "styled-components";
+
+export const StyledParagraph = styled.p`
+  color: #333;
+  font-size: 2rem;
+`;
