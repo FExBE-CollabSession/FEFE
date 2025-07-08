86fd74aff110b643d96499ff573cf8d97478b3f0
kdhdd <kdso10@naver.com>
2025-07-08 13:42:24 +0900

    :recycle: 회원가입 - 로그인 주소 연결

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -78,7 +78,7 @@ export default function SignupPage() {
                     </SignupButton>
                 </Form>
 
-                <LoginLink onClick={() => navigate("/login")}>
+                <LoginLink onClick={() => navigate("/")}>
                     이미 계정이 있으신가요? 로그인하기
                 </LoginLink>
             </SignupCard>
